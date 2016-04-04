4f44c084d6ca3e8bfaf68da1d54c0f9dcbff83e4
Jesus <jfr1013@gmail.com>
2016-04-04 23:51:01 +0200

    Añadida clase Segment

---
+++ Segment.java
@@ -0,0 +1,16 @@
+/**
+ * Write a description of class Segment here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Segment
+{
+    /**
+     * Constructor for objects of class Segment
+     */
+    public Segment()
+    {
+        
+    }
+}
