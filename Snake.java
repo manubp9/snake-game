62a691c6e8acc03789bbaf65bdb3fd85b2f21f01
Jesus <jfr1013@gmail.com>
2016-04-05 10:01:42 +0200

    Añadida clase Snake

---
+++ Snake.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class Snake here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Snake
+{
+    /**
+     * Constructor for objects of class Snake
+     */
+    public Snake()
+    {
+        
+    }
+}
