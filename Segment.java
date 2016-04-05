0c871d5011ffd03bfd9a2029c4b129c4c5b11872
Jesus <jfr1013@gmail.com>
2016-04-05 10:50:04 +0200

    Corregida clase Segment

--- Segment.java
+++ Segment.java
@@ -1,4 +1,4 @@
-import java.awt.*;
+import java.awt.Color;
 import java.awt.geom.*;
 
 /**
@@ -11,109 +11,96 @@ public class Segment
 {
     private int longitud;
     private int direccion;
-    private int posX;
-    private int posY;
+    private int posicionX;
+    private int posicionY;
     private Color color;
-    private Canvas canvas;
-    public static final int LONGITUD_SEGMENTO = 40;
-    private static final int DIFERENCIA_DE_GRADOS_ENTRE_DIRECCIONES = 90;
-    private static final int DIRECCION_DERECHA = 0;
-    private static final int DIRECCION_ABAJO = 1;
-    private static final int DIRECCION_IZQUIERDA = 2;
-    private static final int DIRECCION_ARRIBA = 3;
-    
+    private Canvas lienzo;
+    public static final int LONGITUD_SEGMENTO = 10;
+
     /**
      * Constructor for objects of class Segment
-     * Direcciones válidas: 0 DERECHA, 1 ABAJO, 2 IZQUIERDA, 3 ARRIBA
+     * Direcciones válidas: 0 DERECHA, 90 ABAJO, 180 IZQUIERDA, 270 ARRIBA
      */
-    public Segment(int direccion, int posX, int posY)
+    public Segment(int posX, int posY,int direccion, Color color)
     {
         longitud = LONGITUD_SEGMENTO;
+        posicionX = posX;
+        posicionY = posY;
         this.direccion = direccion;
-        this.posX = posX;
-        this.posY = posY;
-        color = Color.BLACK;
-        canvas = new Canvas("myCanvas",500,500);
-        dibujar();
+        this.color = color;
     }
-    
+
     /**
      * Método que devuelve la posicion inicial en el eje X de un segmento
      */
-    public int getPosInicialX(){
-        return posX;
+    public int getPosicionInicialX(){
+        return posicionX;
     }
-        
+
     /**
      * Método que devuelve la posicion inicial en el eje Y de un segmento
      */
-    public int getPosInicialY(){
-        return posY;
+    public int getPosicionInicialY(){
+        return  posicionY;
     }
-    
+
     /**
      * Método que devuelve la posicion final en el eje X de un segmento
      */
-    public int getPosFinalX(){
-        int posFinalX = posX;
-        if (direccion == DIRECCION_DERECHA){
-            posFinalX = posX + LONGITUD_SEGMENTO;
+    public int getPosicionFinalX(){
+        int posFinalX = posicionX;
+        if (direccion == 0){
+            posFinalX = posicionX + LONGITUD_SEGMENTO;
         }
-        else if (direccion == DIRECCION_IZQUIERDA){
-            posFinalX = posX - LONGITUD_SEGMENTO;
+        else if (direccion == 180){
+            posFinalX = posicionX - LONGITUD_SEGMENTO;
         }
         return posFinalX;
     }
-    
+
     /**
      * Método que devuelve la posicion final en el eje Y de un segmento
      */
-    public int getPosFinalY(){
-        int posFinalY = posY;
-        if (direccion == DIRECCION_ABAJO){
-            posFinalY = posY + LONGITUD_SEGMENTO;
+    public int getPosicionFinalY(){
+        int posFinalY = posicionY;
+        if (direccion == 90){
+            posFinalY = posicionY + LONGITUD_SEGMENTO;
         }
-        else if (direccion == DIRECCION_ARRIBA){
-            posFinalY = posY - LONGITUD_SEGMENTO;
+        else if (direccion == 270){
+            posFinalY = posicionY - LONGITUD_SEGMENTO;
         }
         return posFinalY;
     }
-    
+
     /**
      * Método que devuelve la direccion en la que se ha creado el segmento: 
      * 0º DERECHA, 90º ABAJO, 180º IZQUIERDA, 270º ARRIBA
      */
     public int getDireccion(){
-        return direccion * DIFERENCIA_DE_GRADOS_ENTRE_DIRECCIONES;
+        return direccion;
+    }
+
+    /**
+     * Método que dibuja el segmento en el lienzo
+     */
+    public void dibujar(Canvas lienzo){
+        lienzo.setForegroundColor(color);
+        lienzo.drawLine(posicionX,posicionY,getPosicionFinalX(),getPosicionFinalY());
     }
     
     /**
-     * Método que dibuja un segmento en el lienzo
+     * Método que borra el segmento del lienzo
      */
-    public void dibujar(){
-        canvas.setForegroundColor(color);
-        if (direccion == DIRECCION_DERECHA){
-            canvas.drawLine(posX, posY, posX + LONGITUD_SEGMENTO, posY);
-        }
-        else if (direccion == DIRECCION_ABAJO){
-            canvas.drawLine(posX, posY, posX, posY + LONGITUD_SEGMENTO);
-        }
-        else if (direccion == DIRECCION_IZQUIERDA){
-            canvas.drawLine(posX, posY, posX - LONGITUD_SEGMENTO, posY);
-        }
-        else if (direccion == DIRECCION_ARRIBA){
-            canvas.drawLine(posX, posY, posX, posY - LONGITUD_SEGMENTO);
-        }
+    public void borrar(Canvas lienzo)
+    {
+      lienzo.setForegroundColor(lienzo.getBackgroundColor());
+      lienzo.drawLine(posicionX,posicionY,getPosicionFinalX(),getPosicionFinalY());
     }
-    
+
     /**
      * Método para comprobar si el segmento colisiona con otro
      */
     public boolean colisiona(Segment segmento){
-        boolean colisiona = false;
-        if (getPosFinalX() == segmento.getPosInicialX() || getPosFinalY() == segmento.getPosInicialY()){
-            colisiona = true;
-        }
-        return colisiona;
+        return (posicionX == segmento.getPosicionFinalX() && posicionY == segmento.getPosicionFinalY());
     }
 }
--- Snake.java
+++ Snake.java
@@ -1,3 +1,5 @@
+import java.awt.*;
+import java.util.ArrayList;
 
 /**
  * Write a description of class Snake here.
@@ -7,11 +9,17 @@
  */
 public class Snake
 {
+    private int altoLienzo;
+    private int anchoLienzo;
+    private static final int NUM_SEGMENTOS_INICIAL = 3;
+    private ArrayList<Segment> listaSegmentos;
+    private static final int DIFERENCIA_DE_GRADOS_ENTRE_DIRECCIONES = 90;
+    private static final int MARGEN_LIENZO = 10;
     /**
      * Constructor for objects of class Snake
      */
     public Snake()
     {
-        
+
     }
 }
